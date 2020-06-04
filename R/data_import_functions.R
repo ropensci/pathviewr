49f2fe517cb610986a81b85e10cbe8f951a8a5fb
Vikram Baliga <vbaliga87@gmail.com>
2020-06-04 10:12:38 -0700

    add read_motiv_markers
    copied directly from flightlab/ROM_optitrack. it would be good to combine this with read_motiv_csv() and have one flexible import function to rule them all

--- R/data_import_functions.R
+++ R/data_import_functions.R
@@ -132,6 +132,158 @@ read_motiv_csv <-
   }
 
 
+############################### read_motiv_markers #############################
+## Import data from a CSV and create a `motiv` object
+## Specific to trials in which no rigid bodies were used
+## Basically behaves as a tibble but with metadata stored as attributes
+
+## Note 2020-06-04: it would be good to combine this with read_motiv_csv() and
+## have one flexible import function to rule them all (and in the darkness
+## bind them)
+
+read_motiv_markers <-
+  function(file_name,
+           file_id = NA,
+           ...){
+
+    ## Import checks
+    if (missing(file_name))
+      stop("A file_name is required")
+    if (!file.exists(file_name))
+      stop(paste0("File ", file_name, " not found!"))
+
+    ## Open connection to file for reading in text mode
+    file_con <- file(file_name, "r")
+
+    ## Match file_id to file_name if no file_id is supplied
+    if (is.na(file_id)) file_id <- basename(file_name)
+
+    ## Get maketime of file (may not be accurate...use with caution!)
+    mtime <- file.info(file_name)$mtime
+
+    ## Setup for reading in file
+    header <- c()
+    marker_id <- c()
+    marker_id_line <- c()
+    data_names_part_one <- c()
+    data_names_part_two <- c()
+    data_names <- c()
+
+    ## Read in header (line 1)
+    header <- c(header, readLines(file_con, 1))
+    ## Split the character vector and make a data.frame of it
+    header <-
+      strsplit(header[[1]], ",")[[1]] %>%
+      data.frame(stringsAsFactors = FALSE)
+    ## Convert the 1-col data frame into a 2-col data frame:
+    odds <- seq_along(header$.) %% 2 == 1
+    metadata <- header$.[odds] # odd-numbered rows are metadata
+    value <- header$.[!odds] # even-numbered rows are corresponding values
+    header <- data.frame(metadata, value,
+                         stringsAsFactors = FALSE)
+
+    ## Marker IDs
+    l <- readLines(file_con, 3)
+    ## Save the line itself for use later
+    marker_id_line <- strsplit(l[3], ",")[[1]]
+    ## Make a list of the unique rigid bodies
+    marker_id <- levels(as.factor(marker_id_line[-c(1, 2)]))
+
+    ## Rename Marker IDs and also extract Bird ID
+    namez <- marker_id
+    ##  Regular expressions are the woooooorst
+    bird_id_colon <- stringr::str_extract(namez, ".+?(?<=:)")[1]
+    bird_id <- sub(":", "", bird_id_colon)
+    marker_id <- sub(bird_id_colon, "", namez)
+    marker_id_line <- sub(bird_id_colon, "", marker_id_line)
+
+    ## Might as well add bird_id to header
+    bird_df <- data.frame("Bird ID", bird_id)
+    names(bird_df) <- c("metadata", "value")
+    header <- rbind(header, bird_df)
+
+    ## Data names line 6
+    l <- readLines(file_con, 2)
+    data_names_part_one <- strsplit(l[2], ",")[[1]]
+
+    ## Data names line 7
+    l <- readLines(file_con, 1)
+    data_names_part_two <- strsplit(l, ",")[[1]]
+
+    ## Make variables same length
+    ## Often arises when final column is "Marker Quality" in line 6 with
+    ## nothing in line 7
+    if (length(data_names_part_two) == length(data_names_part_one) - 1) {
+      data_names_part_two <- c(data_names_part_two, "") # add blank character
+    } else {
+      data_names_part_two <- data_names_part_two # keep the same
+    }
+
+    ## Make data frame to help with data names
+    data_names_frame <- data.frame(v1 = marker_id_line,
+                                   v2 = data_names_part_one,
+                                   v3 = data_names_part_two)
+    ## v2 (Position) seems useless, but I'm keeping it in for now and ignoring
+    ## it in the next step
+
+    ## Stitch it together
+    ## Using a for() loop for now; I'm sure purrr::map() can be used instead
+    ## but who cares
+    for (i in 1:nrow(data_names_frame)) {
+      data_names[[i]] <- paste0(data_names_frame[i, 1],
+                                "_",
+                                #data_names_frame[i, 2],
+                                #"_",
+                                data_names_frame[i, 3])
+    }
+
+    ## Some hacky cleanup
+    ## Not ideal but it works
+    data_names <- sub("_Frame", "frame", data_names)
+    data_names <- sub(" ", "_", data_names)
+    data_names <- sub("Name_Time_\\(Seconds\\)", "time_sec", data_names)
+
+    ## Read in data
+    dataz <- data.table::fread(
+      text = readLines(file_con),
+      header = FALSE,
+      sep = ",",
+      dec = ".",
+      stringsAsFactors = FALSE
+    )
+    ## Quickly check for non-numerics and warn if found
+    if (any(!apply(dataz, 2, is.numeric))) {
+      warning("The csv file includes non-numeric data.
+              \nPlease ensure that this is intentional before proceeding.")
+    }
+
+    ## Rename columns
+    colnames(dataz) <- data_names
+
+    ## Close up
+    close(file_con)
+
+    ## Make the object (a tibble)
+    data <- tibble::as_tibble(dataz)
+
+    ## Add "motiv" as a class, which may be useful for package-ization later
+    class(data) <- c(class(data), "motiv")
+
+    ## Add metadata as attributes()
+    attr(data, "file_id") <- file_id
+    attr(data, "file_mtime") <- mtime
+    attr(data, "bird_id") <- bird_id
+    attr(data, "header") <- header
+    attr(data, "marker_ids") <- marker_id
+    attr(data, "data_names") <- data_names
+    attr(data, "d1") <- data_names_part_one
+    attr(data, "d2") <- data_names_part_two
+
+    ## Export
+    return(data)
+  }
+
+
 ################################ get_header_motiv ##############################
 ## Extract header info from imported `motiv` object
 get_header_motiv <-function(obj_name,
