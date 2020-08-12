81d6684b74039b03ed035e768c31a6c0b643c006
epress12 <60632497+epress12@users.noreply.github.com>
2020-08-11 17:26:20 -0700

    Add calc_s_freq with roxygen

--- R/analytical_functions.R
+++ R/analytical_functions.R
@@ -542,6 +542,150 @@ calc_vis_angle <- function(obj_name,
 
 
 
+##########################    calc_s_freq     ###########################
+
+#' Based on rigid body, i.e animal head positions in a "V" shaped tunnel,
+#' \code{calc_s_freq()} calculates how the animal percieves the visual stimuli
+#' in terms of its spatial frequency modulated by distance to the stimulus.
+#'
+#' @param obj_name A tibble or data.frame with attribute \code{viewr}
+#' @param vertex_angle The angle (in degrees) subtended by a vertical axis and
+#' the sides of the tunnel. Equivalent to the angle of the "V" divided by 2.
+#' \code{vertex_angle)} defaults to 45.
+#' @param gnd_plane The vertical distance (in meters) between the bottom of the
+#' "V" and the horizontal plane through the origin (0,0,0).
+#' @param stim_param_pos The length (in meters) of the visual stimulus presented
+#' on the positive side of the tunnel (i.e. \code{position_width >= 0}). For
+#' example, a sinusoidal grating 10cm wide is \code{stim_param_pos = 0.1}
+#' @param stim_param_neg The same convention as \code{stim_param_pos} but for
+#' stimuli presented on the negative side of the tunnel
+#' (i.e. \code{position_width < 0}).
+#' @param simplify_output If TRUE, the returned object includes minimum distance
+#' as well as the visual angles (in degrees) to either side of the
+#' tunnel. If FALSE, the returned object includes all variables internal to the
+#' calculation.
+#'
+#' @details \code{cal_s_freq} assumes fixed gaze at the point on the
+#' either side of the tunnel that minimizes the distance to visual stimuli and
+#' thereby minimizes spatial frequencies.
+#'
+#' @return A tibble or data.frame with added variables for
+#' \code{s_freq_pos} and \code{s_freq_neg} reported in cycles per 1˚ of visual
+#' angle.
+#'
+#' @author Eric R. Press
+#'
+#' @family optic flow functions
+#'
+#' @examples
+#'
+#' @export
+
+
+
+calc_s_freq <- function(obj_name,
+                        gnd_plane,
+                        stim_param_pos,
+                        stim_param_neg,
+                        vertex_angle = 45,
+                        simplify_output = FALSE){
+
+  ## Check that it's a viewr object
+  if (!any(attr(obj_name,"pathviewR_steps") == "viewr")) {
+    stop("This doesn't seem to be a viewr object")
+  }
+
+  ## Translate vertex_angle from degrees to radians for trig functions
+  vertex_angle <- deg2rad(vertex_angle)
+
+  ## duplicate object for simplify = TRUE
+  obj_simplify <- obj_name
+
+  ## Introduce variables for height_2_vertex and height_2_screen
+  obj_name$height_2_vertex <- gnd_plane + obj_name$position_height
+  obj_name$height_2_screen <- obj_name$height_2_vertex -
+    (abs(obj_name$position_width) / tan(vertex_angle))
+
+
+  ## Introduce variables for width_2_screen on positive and negative sides
+  ## of the tunnel.
+  ## width_2_screen refers to the horizontal distance between the bird and
+  ## either screen.
+  obj_name$width_2_screen_pos <-
+    ifelse(obj_name$position_width >= 0, # if in positive side of tunnel
+           obj_name$height_2_screen * tan(vertex_angle), # TRUE
+           (obj_name$height_2_screen * tan(vertex_angle)) +
+             (2 * abs(obj_name$position_width))) # FALSE
+
+  obj_name$width_2_screen_neg <-
+    ifelse(obj_name$position_width < 0, # if in negative side of tunnel
+           obj_name$height_2_screen * tan(vertex_angle), # TRUE
+           (obj_name$height_2_screen * tan(vertex_angle)) +
+             (2 * abs(obj_name$position_width))) # FALSE
+
+
+  ## When the bird is outside the boundaries created by orthogonal planes to
+  ## each screen, erroneous visual angles are calculated based on a
+  ## minimum distance to either screen.
+  ## Therefore min_dist values need to be adjusted according to position_width
+
+  ## create variable of boundary values for each observation
+  obj_name$bound_pos <- obj_name$height_2_vertex * tan(deg2rad(90) -
+                                                         vertex_angle)
+  obj_name$bound_neg <- obj_name$height_2_vertex * -tan(deg2rad(90) -
+                                                          vertex_angle)
+
+
+  ## Introduce variable min_dist on positive and negative sides of the
+  ## tunnel. min_dist refers to the minimum distance between the bird and either
+  ## screen.
+  obj_name$min_dist_pos <-
+    ifelse(obj_name$position_width <= 0 &
+             obj_name$position_width <= obj_name$bound_neg,
+           # if position_width is negative and less than the boundary value
+           sqrt(obj_name$height_2_vertex^2 + obj_name$position_width^2),
+           # return distance to vertex
+           obj_name$width_2_screen_pos * sin(deg2rad(90)- vertex_angle))
+  # return minimum distance to positive screen
+
+  obj_name$min_dist_neg <-
+    ifelse(obj_name$position_width >= 0 &
+             obj_name$position_width >= obj_name$bound_pos,
+           # if position_width is positive and greater than the boundary value
+           sqrt(obj_name$height_2_vertex^2 + obj_name$position_width^2),
+           # return distance to vertex
+           obj_name$width_2_screen_neg * sin(deg2rad(90) - vertex_angle))
+  # return minimum distance to negative screen
+
+
+  ## Calculate distance along plane of screen equal to 1˚ of visual angle.
+  deg_dist_pos <- 2 * obj_name$min_dist_pos * tan(deg2rad(1))
+  deg_dist_neg <- 2 * obj_name$min_dist_neg * tan(deg2rad(1))
+
+  ## Calculate spatial frequency as number of cycles of stimulus per 1˚ of
+  ## visual angle.
+  obj_name$s_freq_pos <- deg_dist_pos / stim_param_pos
+  obj_name$s_freq_neg <- deg_dist_neg / stim_param_neg
+
+
+  ## Create simple data frame by adding min_dist and spatial frequencies
+  obj_simplify$min_dist_pos <- obj_name$min_dist_pos
+  obj_simplify$min_dist_neg <- obj_name$min_dist_neg
+  obj_simplify$s_freq_pos <- obj_name$s_freq_pos
+  obj_simplify$s_freq_neg <- obj_name$s_freq_neg
+
+  ## return simple or complete data table based on simplify argument
+  if(simplify_output == TRUE){
+    return(obj_simplify)
+  } else {
+    return(obj_name)
+  }
+}
+
+
+
+
+
 ## Remaining three functions originally written by Christina Harvey
 ## May adapt for our purposes in the future
 #angles in 3D space
