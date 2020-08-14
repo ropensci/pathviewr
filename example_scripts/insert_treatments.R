c3ab370dc1d152aa4d8157b915e1082e5f8e62f2
epress12 <60632497+epress12@users.noreply.github.com>
2020-08-14 16:18:36 -0700

    update to insert_treatments

--- example_scripts/insert_treatments.R
+++ example_scripts/insert_treatments.R
@@ -1,16 +1,10 @@
-### insert treatments function will be used to add treatment information to a
-### data object by storing it as variables or in the metadata.
-### include vertex position, stimulus parameters,
-###
-##
-### comes after get_full_trajectories
-###
-### tunnel_setup part of attributes as a dataframe with property and value as in
-### header metadata
-###
-###
-###  add argument checks
 
+### With the insert_treatments() function, users can add tunnel set up details
+### and visual stimulus information for each treatment. Currently, it only
+### accepts a single treatment but this will be expanded in the future.
+### Tunnel and stimulus info is also stored in the data object's attributes.
+
+### This function must be run after get_full_trajectories
 
 insert_treatments <- function(obj_name,
                               vertex_height,
@@ -18,7 +12,7 @@ insert_treatments <- function(obj_name,
                               treatment = NULL){
 
   ## Check that it's a viewr object
-  if (!any(attr(obj_name,"pathviewR_steps") == "viewr")) {
+  if (!any(attr(obj_name,"pathviewR_steps") == "viewr")){
     stop("This doesn't seem to be a viewr object")
   }
 
@@ -32,7 +26,7 @@ insert_treatments <- function(obj_name,
   ## position of certain variables remaining constant
   obj_name <- tibble::add_column(obj_name, .before = "frame",
                                  vertex_height = vertex_height,
-                                 vertex_angle = vertex_angle,
+                                 vertex_angle = deg_2_rad(vertex_angle),
                                  treatment = treatment)
 
   ## Add arguments into metadata......surewhynot
@@ -46,29 +40,47 @@ insert_treatments <- function(obj_name,
   obj_name$stim_param_neg <- vector(mode = "numeric", length = nrow(obj_name))
 
 
-  ## Add stimulus parameters based on treatment name
+  ## Add stimulus parameters based on treatment name (will update with actual
+  ## values once I have them)
   # stim_param_pos
   for (i in 1:nrow(obj_name)){
     if (obj_name$treatment[[i]] == "latA"){
-      obj_name$stim_param_pos[[i]] <- 0.1}
-    elseif (obj_name$treatment[[i]] == "latB"){
-      obj_name$stim_param_pos[[i]] <- 0.1}
-    elseif (obj_name$treatment[[i]] == "latC"){
       obj_name$stim_param_pos[[i]] <- 0.1
-    elseif (obj_name$treatment[[i]] == "latD"){
+    } else if (obj_name$treatment[[i]] == "latB"){
+      obj_name$stim_param_pos[[i]] <- 0.1
+    } else if (obj_name$treatment[[i]] == "latC"){
+      obj_name$stim_param_pos[[i]] <- 0.1
+    } else if (obj_name$treatment[[i]] == "latD"){
+      obj_name$stim_param_pos[[i]] <- 0.1
+    } else if (obj_name$treatment[[i]] == "latE"){
+      obj_name$stim_param_pos[[i]] <- 0.1
+    } else if (obj_name$treatment[[i]] == "latF"){
+      obj_name$stim_param_pos[[i]] <- 0.1
+    } else if (obj_name$treatment[[i]] == "latG"){
+      obj_name$stim_param_pos[[i]] <- 0.1
+    } else if (obj_name$treatment[[i]] == "latH"){
       obj_name$stim_param_pos[[i]] <- 0.1
     }
   }
-  # stim_param_neg
+
+  # stim_param_neg (again I will update with real values)
   for (i in 1:nrow(obj_name)){
     if (obj_name$treatment[[i]] == "latA"){
       obj_name$stim_param_neg[[i]] <- 0.1
-    } elseif (obj_name$treatment[[i]] == "latB"){
+    } else if (obj_name$treatment[[i]] == "latB"){
       obj_name$stim_param_neg[[i]] <- 0.1
-    } elseif (obj_name$treatment[[i]] == "latC"){
+    } else if (obj_name$treatment[[i]] == "latC"){
       obj_name$stim_param_neg[[i]] <- 0.1
-    } elseif (obj_name$treatment[[i]] == "latD"){
+    } else if (obj_name$treatment[[i]] == "latD"){
       obj_name$stim_param_neg[[i]] <- 0.1
+    } else if (obj_name$treatment[[i]] == "latE"){
+      obj_name$stim_param_pos[[i]] <- 0.1
+    } else if (obj_name$treatment[[i]] == "latF"){
+      obj_name$stim_param_pos[[i]] <- 0.1
+    } else if (obj_name$treatment[[i]] == "latG"){
+      obj_name$stim_param_pos[[i]] <- 0.1
+    } else if (obj_name$treatment[[i]] == "latH"){
+      obj_name$stim_param_pos[[i]] <- 0.1
     }
   }
 
@@ -83,12 +95,12 @@ insert_treatments <- function(obj_name,
 
 #### testing zone
 
-test <- insert_treatments(jul_29_full,
-                          vertex_height = -0.3855,
-                          vertex_angle = 45,
-                          treatment = "latD")
+t <- insert_treatments(jul_29_full,
+                       vertex_height = -0.3855,
+                       vertex_angle = 45,
+                       treatment = "latD")
 
-View(test)
+View(t)
 
 attributes(test)
 
