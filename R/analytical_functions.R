b4a63dc6cc816adcb020c1fccced8d4288de6695
epress12 <60632497+epress12@users.noreply.github.com>
2020-09-09 17:06:55 -0700

    calc_sf_V to analytical_functions.R

--- R/analytical_functions.R
+++ R/analytical_functions.R
@@ -746,16 +746,14 @@ calc_vis_angle <- function(obj_name,
 #' Estimate spatial frequency of visual stimuli in V-shaped tunnel
 #'
 #' Based on rigid body, i.e animal head positions in a "V" shaped tunnel,
-#' \code{calc_sf_V()} calculates how the animal perceives the visual stimuli
+#' \code{calc_sf_V()} calculates how the animal perceives visual stimuli
 #' in terms of its spatial frequency modulated by distance to the stimulus.
 #'
 #' @param obj_name The input viewr object; a tibble or data.frame with attribute
-#'   \code{pathviewR_steps} that includes \code{"viewr"}
+#' \code{pathviewR_steps} that includes \code{"viewr"}
 #' @param vertex_angle The angle (in degrees) subtended by a vertical axis and
 #' the sides of the tunnel. Equivalent to the angle of the "V" divided by 2.
 #' \code{vertex_angle)} defaults to 45.
-#' @param gnd_plane The vertical distance (in meters) between the bottom of the
-#' "V" and the horizontal plane through the origin (0,0,0).
 #' @param stim_param_pos The length (in meters) of the visual stimulus presented
 #' on the positive side of the tunnel (i.e. \code{position_width >= 0}). For
 #' example, a sinusoidal grating 10cm wide is \code{stim_param_pos = 0.1}
@@ -784,29 +782,27 @@ calc_vis_angle <- function(obj_name,
 #' @export
 
 
-
 calc_sf_V <- function(obj_name,
-                      gnd_plane,
-                      stim_param_pos,
-                      stim_param_neg,
-                      vertex_angle = 45,
-                      simplify_output = FALSE){
+                      simplify_output = FALSE) {
 
   ## Check that it's a viewr object
-  if (!any(attr(obj_name,"pathviewR_steps") == "viewr")) {
+  if (!any(attr(obj_name, "pathviewR_steps") == "viewr")) {
     stop("This doesn't seem to be a viewr object")
   }
 
-  ## Translate vertex_angle from degrees to radians for trig functions
-  vertex_angle <- deg_2_rad(vertex_angle)
+  ## Check that get_full_trajectories has been run
+  if (!any(attr(obj_name, "pathviewR_steps") == "treatments_added")) {
+    stop("Run insert_treatments() prior to use.")
+  }
 
   ## duplicate object for simplify = TRUE
   obj_simplify <- obj_name
 
   ## Introduce variables for height_2_vertex and height_2_screen
-  obj_name$height_2_vertex <- gnd_plane + obj_name$position_height
+  obj_name$height_2_vertex <-
+    abs(obj_name$vertex_height) + obj_name$position_height
   obj_name$height_2_screen <- obj_name$height_2_vertex -
-    (abs(obj_name$position_width) / tan(vertex_angle))
+    (abs(obj_name$position_width) / tan(obj_name$vertex_angle))
 
 
   ## Introduce variables for width_2_screen on positive and negative sides
@@ -814,16 +810,24 @@ calc_sf_V <- function(obj_name,
   ## width_2_screen refers to the horizontal distance between the bird and
   ## either screen.
   obj_name$width_2_screen_pos <-
-    ifelse(obj_name$position_width >= 0, # if in positive side of tunnel
-           obj_name$height_2_screen * tan(vertex_angle), # TRUE
-           (obj_name$height_2_screen * tan(vertex_angle)) +
-             (2 * abs(obj_name$position_width))) # FALSE
+    ifelse(
+      obj_name$position_width >= 0,
+      # if in positive side of tunnel
+      obj_name$height_2_screen * tan(obj_name$vertex_angle),
+      # TRUE
+      (obj_name$height_2_screen * tan(obj_name$vertex_angle)) +
+        (2 * abs(obj_name$position_width))
+    ) # FALSE
 
   obj_name$width_2_screen_neg <-
-    ifelse(obj_name$position_width < 0, # if in negative side of tunnel
-           obj_name$height_2_screen * tan(vertex_angle), # TRUE
-           (obj_name$height_2_screen * tan(vertex_angle)) +
-             (2 * abs(obj_name$position_width))) # FALSE
+    ifelse(
+      obj_name$position_width < 0,
+      # if in negative side of tunnel
+      obj_name$height_2_screen * tan(obj_name$vertex_angle),
+      # TRUE
+      (obj_name$height_2_screen * tan(obj_name$vertex_angle)) +
+        (2 * abs(obj_name$position_width))
+    ) # FALSE
 
 
   ## When the bird is outside the boundaries created by orthogonal planes to
@@ -831,33 +835,35 @@ calc_sf_V <- function(obj_name,
   ## minimum distance to either screen.
   ## Therefore min_dist values need to be adjusted according to position_width
 
-  ## create variable of boundary values for each observation
-  obj_name$bound_pos <- obj_name$height_2_vertex * tan(deg_2_rad(90) -
-                                                         vertex_angle)
-  obj_name$bound_neg <- obj_name$height_2_vertex * -tan(deg_2_rad(90) -
-                                                          vertex_angle)
+  ## Create variable of boundary values for each observation
+  obj_name$bound_pos <-
+    obj_name$height_2_vertex * tan(deg_2_rad(90) - obj_name$vertex_angle)
+  obj_name$bound_neg <-
+    obj_name$height_2_vertex * -tan(deg_2_rad(90) - obj_name$vertex_angle)
 
 
   ## Introduce variable min_dist on positive and negative sides of the
   ## tunnel. min_dist refers to the minimum distance between the bird and either
   ## screen.
   obj_name$min_dist_pos <-
-    ifelse(obj_name$position_width <= 0 &
-             obj_name$position_width <= obj_name$bound_neg,
-           # if position_width is negative and less than the boundary value
-           sqrt(obj_name$height_2_vertex^2 + obj_name$position_width^2),
-           # return distance to vertex
-           obj_name$width_2_screen_pos * sin(deg_2_rad(90)- vertex_angle))
-  # return minimum distance to positive screen
+    ifelse(
+      obj_name$position_width <= 0 &
+        obj_name$position_width <= obj_name$bound_neg,
+      # if position_width is negative and less than the boundary value
+      sqrt(obj_name$height_2_vertex^2 + obj_name$position_width^2),
+      # return distance to vertex
+      obj_name$width_2_screen_pos * sin(deg_2_rad(90) - obj_name$vertex_angle))
+      # return minimum distance to positive screen
 
   obj_name$min_dist_neg <-
-    ifelse(obj_name$position_width >= 0 &
-             obj_name$position_width >= obj_name$bound_pos,
-           # if position_width is positive and greater than the boundary value
-           sqrt(obj_name$height_2_vertex^2 + obj_name$position_width^2),
-           # return distance to vertex
-           obj_name$width_2_screen_neg * sin(deg_2_rad(90) - vertex_angle))
-  # return minimum distance to negative screen
+    ifelse(
+      obj_name$position_width >= 0 &
+        obj_name$position_width >= obj_name$bound_pos,
+      # if position_width is positive and greater than the boundary value
+      sqrt(obj_name$height_2_vertex^2 + obj_name$position_width ^2),
+      # return distance to vertex
+      obj_name$width_2_screen_neg * sin(deg_2_rad(90) - obj_name$vertex_angle))
+      # return minimum distance to negative screen
 
 
   ## Calculate distance along plane of screen equal to 1˚ of visual angle.
@@ -866,23 +872,29 @@ calc_sf_V <- function(obj_name,
 
   ## Calculate spatial frequency as number of cycles of stimulus per 1˚ of
   ## visual angle.
-  obj_name$s_freq_pos <- deg_dist_pos / stim_param_pos
-  obj_name$s_freq_neg <- deg_dist_neg / stim_param_neg
+  obj_name$sf_pos <- deg_dist_pos / obj_name$stim_param_pos
+  obj_name$sf_neg <- deg_dist_neg / obj_name$stim_param_neg
 
 
   ## Create simple data frame by adding min_dist and spatial frequencies
   obj_simplify$min_dist_pos <- obj_name$min_dist_pos
   obj_simplify$min_dist_neg <- obj_name$min_dist_neg
-  obj_simplify$s_freq_pos <- obj_name$s_freq_pos
-  obj_simplify$s_freq_neg <- obj_name$s_freq_neg
+  obj_simplify$sf_pos <- obj_name$sf_pos
+  obj_simplify$sf_neg <- obj_name$sf_neg
+
+  ## Leave note that spatial frequencies were calculated on dataset
+  attr(obj_name, "pathviewR_steps") <-
+    c(attr(obj_name, "pathviewR_steps"),
+      "sf_calculated")
 
   ## return simple or complete data table based on simplify argument
-  if(simplify_output == TRUE){
+  if (simplify_output == TRUE) {
     return(obj_simplify)
   } else {
     return(obj_name)
   }
 }
+
 
 
 ##################################  calc_sf_box   ##############################
