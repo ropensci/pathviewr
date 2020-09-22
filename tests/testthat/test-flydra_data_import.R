1ec22f61ed208e8f238c6efe2e107c7299f3cbeb
Vikram Baliga <vbaliga87@gmail.com>
2020-09-21 22:03:20 -0700

    flesh out testthat for flydra_data_import

--- tests/testthat/test-flydra_data_import.R
+++ tests/testthat/test-flydra_data_import.R
@@ -1,8 +1,17 @@
-context("importing flydra mat files")
+## Tests of read_flydra_data()
 
 ## Pre-import tests
 test_that(
-  "read_flydra_mat stops when no subject is supplied",
+  "read_flydra_mat() fails when no file is supplied",
+  {
+    expect_error(
+      read_flydra_mat(subject_name = "birdie_wooster"),
+      "A mat_file is required"
+    )
+  }
+)
+test_that(
+  "read_flydra_mat() stops when no subject is supplied",
   {
     expect_error(
       read_flydra_mat(
@@ -12,14 +21,16 @@ test_that(
       "A subject_name is required")
 })
 
-## Import the file for other tests
+
+## Import the file for other tests (see below)
 flydra_test_data <-
   read_flydra_mat(system.file("extdata", "pathviewR_flydra_example_data.mat",
                              package = 'pathviewR'),
                   subject_name = "birdie_wooster")
 
 
-test_that("read_flydra_mat reads data in correctly", {
+## Test that imported files have the correct structure
+test_that("read_flydra_mat() reads data in correctly", {
   ## Check that the column names appear correctly
   expect_equal(names(flydra_test_data), c("frame", "time_sec", "subject",
                                           "position_length", "position_width",
