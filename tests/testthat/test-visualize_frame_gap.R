60588109600bc6b18d4fe83336023f5f19315631
Melissa <armstrong@zoology.ubc.ca>
2020-09-25 15:44:13 -0700

    starting tests for visualize_frame_gap

---
+++ tests/testthat/test-visualize_frame_gap.R
@@ -0,0 +1,31 @@
+# Tests of visualize_frame_gap_choice()
+
+## Import the example Motive data included in the package
+motive_data <-
+  read_motive_csv(system.file("extdata", "pathviewR_motive_example_data.csv",
+                              package = 'pathviewR'))
+
+motive_selected <-
+  motive_data %>%
+  relabel_viewr_axes() %>%
+  gather_tunnel_data() %>%
+  trim_tunnel_outliers() %>%
+  rotate_tunnel() %>%
+  get_velocity() %>%
+  select_x_percent(desired_percent = 50)
+
+visualize_frame_gap_choice(motive_selected, loops = 10)
+
+# make a bunch of empty vectors to dump info
+mfg <- vector("list", 10)
+cts <- vector("list", 10)
+trajectory_count <- vector(mode = "double", 10)
+frame_gap_allowed <- vector(mode = "double", 10)
+
+#test empty vectors
+test_that("vectors are empty before looping", {
+  expect_equal(mfg[[3]], NULL)
+  expect_equal(cts[[10]], NULL)
+  expect_equal(trajectory_count[[6]], 0)
+  expect_equal(frame_gap_allowed[[4]], 0)
+})
