a0eb0377c28dc2079d4ae11551722ee0b5cb1861
Melissa <armstrong@zoology.ubc.ca>
2020-09-30 10:22:31 -0700

    adding vdiffr to all plotting functions

--- tests/testthat/test-plot_by_subject.R
+++ tests/testthat/test-plot_by_subject.R
@@ -16,9 +16,26 @@ motive_full <-
 
 #test plot output w/vdiffr
 #use addins to open shiny app to validate plots
-test_that("plot_by_subject() plot output is OK", {
-  vdiffr::expect_doppelganger("plot by subject plot1",
+test_that("plot_by_subject() default plot output is OK", {
+  vdiffr::expect_doppelganger("plot by subject default 1",
                               plot_by_subject(motive_full)[[1]])
-  vdiffr::expect_doppelganger("plot by subject plot2",
+  vdiffr::expect_doppelganger("plot by subject default 2",
                               plot_by_subject(motive_full)[[2]])
+})
+
+## Add treatment information
+motive_full$treatment <- c(rep("latA", 100), rep("latB", 100),
+                           rep("latA", 100), rep("latB", 149))
+
+## Plot all trajectories by subject, color by treatment
+motive_full %>%
+  plot_by_subject(col_by_treat = TRUE)
+
+#test plot output w/vdiffr
+#use addins to open shiny app to validate plots
+test_that("plot_by_subject() default plot output is OK", {
+  vdiffr::expect_doppelganger("plot by subject colbytreat 1",
+                              plot_by_subject(motive_full, col_by_treat = TRUE)[[1]])
+  vdiffr::expect_doppelganger("plot by subject colbytreat 2",
+                              plot_by_subject(motive_full, col_by_treat = TRUE)[[2]])
 })
--- tests/testthat/test-plot_viewr_trajectories.R
+++ tests/testthat/test-plot_viewr_trajectories.R
@@ -15,6 +15,28 @@ motive_full <-
 #test plot output w/vdiffr
 #use addins to open shiny app to validate plots
 test_that("plot_viewr_trajectories() plot output is OK", {
-  vdiffr::expect_doppelganger("viewr trajs multi plot",
-                      plot_viewr_trajectories(motive_full, multi_plot = TRUE))
+  vdiffr::expect_doppelganger("multi plot default",
+                      plot_viewr_trajectories(motive_full,
+                                              plot_axes = c("length", "width"),
+                                              multi_plot = TRUE))
+  vdiffr::expect_doppelganger("multi plot lh",
+                              plot_viewr_trajectories(motive_full,
+                                                      plot_axes = c("length", "height"),
+                                                      multi_plot = TRUE))
+  vdiffr::expect_doppelganger("multi plot wl",
+                              plot_viewr_trajectories(motive_full,
+                                                      plot_axes = c("width", "length"),
+                                                      multi_plot = TRUE))
+  vdiffr::expect_doppelganger("multi plot wh",
+                              plot_viewr_trajectories(motive_full,
+                                                      plot_axes = c("width", "height"),
+                                                      multi_plot = TRUE))
+  vdiffr::expect_doppelganger("multi plot hl",
+                              plot_viewr_trajectories(motive_full,
+                                                      plot_axes = c("height", "length"),
+                                                      multi_plot = TRUE))
+  vdiffr::expect_doppelganger("multi plot hw",
+                              plot_viewr_trajectories(motive_full,
+                                                      plot_axes = c("height", "width"),
+                                                      multi_plot = TRUE))
 })
