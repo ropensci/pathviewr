f9de9d7ff286ab7d3edc9356af9ee58a272ab56b
Vikram Baliga <vbaliga87@gmail.com>
2020-09-30 21:30:49 -0700

    try seeing if github install fixes vdiffr bug on travis?

--- tests/testthat/test-plot_viewr_trajectories.R
+++ tests/testthat/test-plot_viewr_trajectories.R
@@ -50,29 +50,29 @@ test_that("plot info (axes, titles, etc.) is correct", {
 
 #test plot output w/vdiffr
 #use addins to open shiny app to validate plots
-# test_that("plot_viewr_trajectories() plot output is OK", {
-#   vdiffr::expect_doppelganger("multi plot default",
-#                       plot_viewr_trajectories(motive_full,
-#                                               plot_axes = c("length", "width"),
-#                                               multi_plot = TRUE))
-#   vdiffr::expect_doppelganger("multi plot lh",
-#                               plot_viewr_trajectories(motive_full,
-#                                                       plot_axes = c("length", "height"),
-#                                                       multi_plot = TRUE))
-#   vdiffr::expect_doppelganger("multi plot wl",
-#                               plot_viewr_trajectories(motive_full,
-#                                                       plot_axes = c("width", "length"),
-#                                                       multi_plot = TRUE))
-#   vdiffr::expect_doppelganger("multi plot wh",
-#                               plot_viewr_trajectories(motive_full,
-#                                                       plot_axes = c("width", "height"),
-#                                                       multi_plot = TRUE))
-#   vdiffr::expect_doppelganger("multi plot hl",
-#                               plot_viewr_trajectories(motive_full,
-#                                                       plot_axes = c("height", "length"),
-#                                                       multi_plot = TRUE))
-#   vdiffr::expect_doppelganger("multi plot hw",
-#                               plot_viewr_trajectories(motive_full,
-#                                                       plot_axes = c("height", "width"),
-#                                                       multi_plot = TRUE))
-# })
+test_that("plot_viewr_trajectories() plot output is OK", {
+  vdiffr::expect_doppelganger("multi plot default",
+                      plot_viewr_trajectories(motive_full,
+                                              plot_axes = c("length", "width"),
+                                              multi_plot = TRUE))
+  vdiffr::expect_doppelganger("multi plot lh",
+                              plot_viewr_trajectories(motive_full,
+                                                      plot_axes = c("length", "height"),
+                                                      multi_plot = TRUE))
+  vdiffr::expect_doppelganger("multi plot wl",
+                              plot_viewr_trajectories(motive_full,
+                                                      plot_axes = c("width", "length"),
+                                                      multi_plot = TRUE))
+  vdiffr::expect_doppelganger("multi plot wh",
+                              plot_viewr_trajectories(motive_full,
+                                                      plot_axes = c("width", "height"),
+                                                      multi_plot = TRUE))
+  vdiffr::expect_doppelganger("multi plot hl",
+                              plot_viewr_trajectories(motive_full,
+                                                      plot_axes = c("height", "length"),
+                                                      multi_plot = TRUE))
+  vdiffr::expect_doppelganger("multi plot hw",
+                              plot_viewr_trajectories(motive_full,
+                                                      plot_axes = c("height", "width"),
+                                                      multi_plot = TRUE))
+})
