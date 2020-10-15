d0d255a3386e70c12cba92ace4b73c1933427875
Vikram Baliga <vbaliga87@gmail.com>
2020-10-14 20:01:16 -0700

    add a couple more tests

---
+++ tests/testthat/test-get_header.R
@@ -0,0 +1,17 @@
+## Tests of get_header_viewr() are in this file
+
+test_that("get_header_viewr() fails when nonsense is supplied", {
+  expect_error(get_header_viewr("steve"))
+  expect_error(get_header_viewr(c("a", "b", "c")))
+})
+
+## Set up for tests of the function value return
+## Import the example Motive data included in the package
+motive_data <-
+  read_motive_csv(system.file("extdata", "pathviewR_motive_example_data.csv",
+                              package = 'pathviewR'))
+
+## Test that attributes are read properly
+test_that("get_header_viewr() parses header info correctly", {
+  expect_equal(get_header_viewr(motive_data)[3,1], "Take Notes")
+})
