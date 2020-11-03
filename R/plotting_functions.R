aae241a2038d651cf2d072366a1b0ad59ea219fc
Vikram Baliga <vbaliga87@gmail.com>
2020-11-02 21:30:51 -0800

    wrap plot_by_subject example in if (interactive())
    this should suppress automatically running it during checks

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -328,34 +328,39 @@ plot_viewr_trajectories <- function(obj_name,
 #' @export
 #' @author Melissa S. Armstrong
 #' @family plotting functions
+#'
 #' @examples
 #' library(pathviewR)
 #' library(ggplot2)
 #' library(magrittr)
 #'
-#' ## Import the example Motive data included in the package
-#' motive_data <-
-#'   read_motive_csv(system.file("extdata", "pathviewR_motive_example_data.csv",
-#'                               package = 'pathviewR'))
+#' if (interactive()) {
+#'   ## Import the example Motive data included in the package
+#'   motive_data <-
+#'     read_motive_csv(system.file("extdata",
+#'                                 "pathviewR_motive_example_data.csv",
+#'                                 package = 'pathviewR'))
 #'
-#' ## Clean, isolate, and label trajectories
-#' motive_full <-
-#'   motive_data %>%
-#'   clean_viewr(desired_percent = 50,
-#'               max_frame_gap = "autodetect",
-#'               span = 0.95)
+#'   ## Clean, isolate, and label trajectories
+#'   motive_full <-
+#'     motive_data %>%
+#'     clean_viewr(desired_percent = 50,
+#'                 max_frame_gap = "autodetect",
+#'                 span = 0.95)
 #'
-#' ## Plot all trajectories by subject
-#' motive_full %>%
-#' plot_by_subject()
+#'   ## Plot all trajectories by subject
+#'   motive_full %>%
+#'     plot_by_subject()
 #'
-#' ## Add treatment information
-#' motive_full$treatment <- c(rep("latA", 100), rep("latB", 100),
-#'                            rep("latA", 100), rep("latB", 149))
+#'   ## Add treatment information
+#'   motive_full$treatment <- c(rep("latA", 100), rep("latB", 100),
+#'                              rep("latA", 100), rep("latB", 149))
 #'
-#' ## Plot all trajectories by subject, color by treatment
-#' motive_full %>%
-#' plot_by_subject(col_by_treat = TRUE)
+#'   ## Plot all trajectories by subject, color by treatment
+#'   motive_full %>%
+#'     plot_by_subject(col_by_treat = TRUE)
+#' }
+
 
 plot_by_subject <- function(obj_name,
                             col_by_treat = FALSE,
